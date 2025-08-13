bf2ea163b115e8b1c3ecf241aaedcee53e7bce83
Vedant Kumar <vedantvisoliya@gmail.com>
2025-08-13 20:49:06 +0530

    added routes for the loggedin and loggedout routes and changed the favicon icon for the web,

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userModelProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          user?.name ?? "No Name",
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/core/error_text.dart';
+import 'package:reddit_clone/core/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/firebase_options.dart';
+import 'package:reddit_clone/models/user_model.dart';
+import 'package:reddit_clone/routes.dart';
 import 'package:reddit_clone/themes/pallete.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,16 +19,49 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref.watch(authControllerProvider.notifier).getUserData(data.uid).first;
+    ref.read(userModelProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Reddit Clone',
-      theme: Pallete.darkModeAppTheme,
-      home: const LoginScreen(),
-    );
+    return ref
+      .watch(authStateChangeProvider)
+      .when(
+        data: (data) => MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: "Reddit Clone",
+          theme: Pallete.darkModeAppTheme,
+          routerDelegate: RoutemasterDelegate(
+            routesBuilder: (context) {
+
+              if (data != null) {
+                getData(ref, data);
+                if (userModel != null) {
+                  return loggedInRoute;
+                }
+              }
+              return loggedOutRoute;
+
+            }
+          ),
+          routeInformationParser: RoutemasterParser(),
+        ), 
+        error: (error, stackTrace) => ErrorText(error: error.toString()), 
+        loading: () => Loader(),
+      );
   }
 }
